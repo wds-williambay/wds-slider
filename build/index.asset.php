5d00b1f143a0c163fb66f69153602580217d0cf9
William Bay <william.bay@webdevstudios.com>
2023-07-10 14:04:48 -0700

    Forgot paginationEl attribute. Thanks to Marty for spotting.

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'e8ac2caf955b9010756d');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '2224b20596590d457417');
